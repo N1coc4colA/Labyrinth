1e8c54845ce44324f3b3aaa146eb8da127325b14
th0to <82789984+th0to@users.noreply.github.com>
2022-03-05 11:20:48 -1000

    Add files via upload

--- data/laby_info.py
+++ data/laby_info.py
@@ -4,33 +4,43 @@ class plato :
     def __init__(self):
         self.hight = 7
         self.width = 7
-        self.board = [[Tile(True, i+j*7, None, 0) for j in range(self.width)]for i in range(self.hight)]
+        self.board = [[Tile(True, i+j*7, 'line', None, 0) for j in range(self.width)]for i in range(self.hight)]
         for i in range(self.hight) :
             for j in range(self.widht) :
-                if i == 0 and j == 0 :
+                if i == 0 and j == 0 :                  # initialisation of spwan 1 
                     self.board[i][j].modif_stat(False)
                     self.board[i][j].modif_color(1)
-                elif i == 0 and j == 7 :
+                    self.board[i][j].modif_raod('angle')
+                    
+                elif i == 0 and j == 7 :                  # initialisation of spwan 2
                     self.board[i][j].modif_stat(False)
                     self.board[i][j].modif_color(2)
-                elif i == 7 and j == 0 :
+                    self.board[i][j].modif_raod('angle')
+                    
+                elif i == 7 and j == 0 :                  # initialisation of spwan 3
                     self.board[i][j].modif_stat(False)
                     self.board[i][j].modif_color(3)
-                elif i == 7 and j == 7 :
+                    self.board[i][j].modif_raod('angle')
+                    
+                elif i == 7 and j == 7 :                  # initialisation of spwan 4
                     self.board[i][j].modif_stat(False)
                     self.board[i][j].modif_color(4)
-                elif self.board[i][j].get_id()%2 == 1 :
+                    self.board[i][j].modif_raod('angle')
+                    
+                elif self.board[i][j].get_id()%2 == 1 :  # initialisation des cases inpaires
                     self.board[i][j].modif_stat(False)
+                    self.board[i][j].modif_raod('triple')
                     
                     
                 
 
 class Tile :
-    def __init__(self, fix, ID, objet, color) : #tourner et road
+    def __init__(self, fix, ID, road, objet, color) : #|||||||||||||||||||tourner||||||||||||||||||||||
         self.item = objet
-        self.stat = fix #True = peut bouger et False = reste fix
+        self.stat = fix # | True = peut bouger | False = reste fix  |
         self.spwan = color
         self._id = ID
+        self.road = road #  |  'line' = ligne droite  |  'angle' = virage  |  'triple' = triple sens  |
     
     def get_id(self) :
         return self._id
@@ -44,6 +54,9 @@ class Tile :
     def get_spawn(self) :
         return self.spawn
     
+    def get_road(self) :
+        return self.road
+    
     def modif_item(self, item) :
         self.item = item
     
@@ -52,3 +65,9 @@ class Tile :
     
     def modif_color(self, color) :
         self.color = color
+        
+    def mofi_road(self, road) :
+        self.road = road
+        
+        
+game = plato
