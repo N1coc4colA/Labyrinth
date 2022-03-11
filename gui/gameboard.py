374c802707cba507e7b00a4200f93f993915f428
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2022-03-11 07:26:21 -1000

    Add files via upload

--- gui/gameboard.py
+++ gui/gameboard.py
@@ -2,29 +2,58 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-from tile import Tile
+from gui.tile import Tile, InputTile
+
+class TestData:
+	def __init__(self, o = 0):
+		self.orientation = o
 
 class Board(QWidget):
 	def __init__(self, parent = None):
 		super(Board, self).__init__(parent)
-		self.setMinimumSize(490, 490)
+		self.setMinimumSize(630, 630)
 		self.tiles = []
+		self.input_tiles = []
 
 	def load(self):
+		#Fill with the main tiles
 		for x in range(7):
 			l = []
 			for y in range(7):
-				t = Tile(self)
-				t.move(x*70, y*70)
+				t = Tile(TestData(), self)
+				t.move(x*70 +70, y*70 +70)
 				l.append(t)
 			self.tiles.append(l)
 
+		#Add the input tiles
+		for i in range(7):
+			if i%2 == 1:
+				l = InputTile(TestData(1), self)
+				r = InputTile(TestData(3), self)
+				l.move(0, i*70 +70)
+				r.move(540, i*70 +70)
+				self.input_tiles.append(l)
+				self.input_tiles.append(r)
+		for i in range(7):
+			if i%2 == 1:
+				u = InputTile(TestData(2), self)
+				d = InputTile(TestData(), self)
+				u.move(i*70 +70, 0)
+				d.move(i*70 +70, 540)
+				self.input_tiles.append(u)
+				self.input_tiles.append(d)
+
+		#Add the draggable tile
+		t = Tile(TestData(), self)
+		t.setMovable(True)
+		t.move(50, 50)
+
 	def resizeEvent(self, event):
-		w = (event.size().width() - 490)/2
-		h = (event.size().height() - 490)/2
+		w = (event.size().width() - 630)/2
+		h = (event.size().height() - 630)/2
 
 		#Move all tiles according to the new size.
 		for x in range(7):
 			for y in range(7):
-				self.tiles[x][y].move(x*70+w, y*70+h)
+				self.tiles[x][y].move(x*70+70+w, y*70+70+h)
 		super(Board, self).resizeEvent(event)
--- gui/tile.py
+++ gui/tile.py
@@ -1,9 +1,20 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+def ensureRaised(w):
+	#Ensures this draggable is the one at the top-front, so it is no hidden when dragged. Max tiles is 9, plus the congrats popup, so do it 9-1 times.
+	for i in range(9):
+		w.raise_()
+
+def widgetAt(p, pos):
+	for w in p.children():
+		if (p.x() <= pos.x() and p.x()+p.width() >= pos.x()) and (p.y() <= pos.y() and p.y()+p.height() >= pos.y()) and w != p:
+			return p
 
 class Tile(QLabel):
-	def __init__(self, parent = None):
+	def __init__(self, d, parent = None):
 		super(Tile, self).__init__(parent)
 		self.setFixedSize(70, 70)
 		self.setFrameStyle(QFrame.Box | QFrame.Raised)
@@ -11,3 +22,76 @@ class Tile(QLabel):
 		self.setMidLineWidth(3)
 		self.setMouseTracking(True)
 		self.setText("A")
+
+		self.movable = False
+		self.press = False
+		self.diff = (0, 0)
+		self.source = QPointF(0, 0)
+		self._id = -1
+		self._data = d
+
+	def internalData(self, d):
+		self._data = d
+		self.update()
+
+	def setMovable(self, mv):
+		self.movable = mv
+		self.press = False
+		self.move(self.source.x(), self.source.y())
+
+	def mouseMoveEvent(self, event):
+		if self.press and self.movable:
+			#Track to move
+			upd = self.mapToParent(event.pos())
+			final = QPoint(upd.x() - self.diff[0], upd.y() - self.diff[1])
+			self.move(final)
+		return super(Tile, self).mouseMoveEvent(event)
+
+	def moveEvent(self, event):
+		if not self.press:
+			#Update when the move is a 'normal' one, used while changing game board with anims mainly.
+			self.source = self.mapToParent(self.pos())
+		return super(Tile, self).moveEvent(event)
+
+	def mousePressEvent(self, event):
+		if self.isEnabled() and self.movable:
+			ensureRaised(self) #Shows the widget at top
+			self.press = True
+			self.source = self.pos()
+			upd = self.mapToParent(event.pos())
+			self.diff = (upd.x() - self.pos().x(), upd.y() - self.pos().y())
+		return super(Tile, self).mousePressEvent(event)
+
+	def mouseReleaseEvent(self, event):
+		if self.press and self.movable:
+			self.press = False
+			upd = self.mapToParent(event.pos())
+			return super(Tile, self).mouseReleaseEvent(event)
+
+class InputTile(Tile):
+
+	def __init__(self, d, parent = None):
+		super(InputTile, self).__init__(parent)
+		self.tile_hovered = False
+		poly = QPolygonF()
+		poly << QPointF(35, 10) << QPointF(10, 60) << QPointF(60, 60)
+		self._path = QPainterPath()
+		self._path.addPolygon(poly)
+		self.setText("B")
+		"""mx = QtGui.QMatrix()
+		mx.rotate({0: 0, 1: 90, 2: 180, 3: 270}[d.orientation])
+		self._path = mx.map(self._path)"""
+
+	def mouseMoveEvent(self, event):
+		#Get the upper tile
+		upd = self.mapToParent(event.pos())
+		self.tile_hovered = (self.x() <= upd.x() and self.x()+self.width() >= upd.x()) and (self.y() <= upd.y() and self.y()+self.height() >= upd.y())
+
+	def paintEvent(self, ev):
+		painter = QPainter(self)
+		painter.fillRect(ev.rect(), QBrush(Qt.green))
+		if self.tile_hovered:
+			painter.fillRect(ev.rect(), QBrush(Qt.green))
+		painter.fillPath(self._path, QBrush(Qt.yellow))
+		return super(InputTile, self).paintEvent(ev)
+
