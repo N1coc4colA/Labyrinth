105dde2bdb08e5fd04ca13140075eb53383ef4fd
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2022-03-25 08:48:15 -1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from PyQt5.QtWidgets import QApplication
-from gameboard import Board
+#This file looks like shit due to python itself. Pitty for it.
 
-if __name__ == '__main__':
-	import sys
-	#Setup the app and show the window.
-	app = QApplication(sys.argv)
-	b = Board()
-	b.load()
-	b.show()
-	sys.exit(app.exec_())
+from PyQt5.QtWidgets import QApplication
+import sys
+app = QApplication(sys.argv)
+
+from gui.gameboard import Board
+from data.laby_info import Plato
+
+bkd = Plato(2)
+b = Board()
+b.load()
+b.show()
+b.setTilesData(bkd.board)
+sys.exit(app.exec())
